40667b920c497b38a595c2336a4e8b592f39df65
Andrei-Zhuk <zhuk_andrey@tut.by>
2020-12-06 14:45:04 +0300

    feat: finish task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://syf6yq0ojc.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://syf6yq0ojc.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://syf6yq0ojc.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://5mpx3zbdoh.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://syf6yq0ojc.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
